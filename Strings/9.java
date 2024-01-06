5b7cb363f62dcf56bbd3f831a3f7a83969f22961
Nandini-Dhanalakota <140742261+Nandini-Dhanalakota@users.noreply.github.com>
2024-01-06 12:31:20 +0530

    Create 9.java

---
+++ Strings/9.java
@@ -0,0 +1,44 @@
+//Leetcode - 1678
+//Goal Parser Interpretation
+
+class Solution {
+    public String interpret(String command) {
+        command = command.replace("(al)", "al").replace("()", "o");
+        return command;
+    }
+}
+
+//(or)
+
+class Solution {
+    public String interpret(String command) {
+                int i = 0;
+        StringBuilder answ = new StringBuilder();
+        while (i < command.length())
+        {
+            switch (command.charAt(i))
+            {
+                case 'G':
+                {
+                    answ.append("G");
+                    i++;
+                    break;
+                }
+                case '(':
+                {
+                    if (command.charAt(i + 1) == ')')
+                    {
+                        i += 2;
+                        answ.append("o");
+                    }
+                    else
+                    {
+                        i += 4;
+                        answ.append("al");
+                    }
+                }
+            }
+        }
+        return answ.toString();
+    }
+}
